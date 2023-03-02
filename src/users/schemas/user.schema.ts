243857457b073606732d741cf249372e8654cd54
fnder7 <matt.nolimiteron@gmail.com>
2023-03-02 18:07:08 +0300

    did it

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -6,7 +6,7 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
-    @Prop({required: true, unique: true})
+    @Prop({ type: String, default: uuidv4 })
     uuid: string;
 
     @Prop({ required: true, unique: true })
