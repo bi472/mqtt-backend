fa3d6b79559f78c9b29a87925950ca71602a5ec7
fnder7 <matt.nolimiteron@gmail.com>
2023-03-02 17:58:48 +0300

    third commit

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop({required: true, unique: true})
+    uuid: string;
+
+    @Prop({ required: true, unique: true })
+    username: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop()
+    refreshToken: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
