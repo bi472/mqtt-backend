f431ff4c25852a29f1864139c3d02d915813e0a3
fnder7 <matt.nolimiteron@gmail.com>
2023-03-26 15:59:11 +0300

    mqtt controller and service upgrade

--- src/mqtt/mqtt.controller.ts
+++ src/mqtt/mqtt.controller.ts
@@ -17,31 +17,33 @@ export class MqttController {
     private readonly templatesService: TemplatesService
   ) {}
 
-
-
   @UseGuards(AccessTokenGuard)
   @Get('connect')
   async connect(
     @Req() req: Request,
     @Body() body: { mqttOptionsID: string , mqttOptionsDto: MqttOptionsDto }
-  ): Promise<any>{
-    const promise =  new Promise(
+  ): Promise<string>{
+    if (this.mqttService.checkConnection()) return `You are already connected.`
+    const promise =  new Promise<string>(
       async (resolve, reject) => {
         setTimeout(() => {resolve(`The waiting time has expired.`)}, 20000)
-        if (this.mqttService.checkConnection()) return `You are already connected to the server`
         const mqttOptionsDto = await this.mqttOptionsService.findUserMqttOptionsByID(body.mqttOptionsID, req.user['sub'])
-        console.log(body.mqttOptionsID, req.user['sub'])
-        this.mqttService.connect(mqttOptionsDto ? mqttOptionsDto : body.mqttOptionsDto, 
-          (connected: boolean, errorMessage: string) => {
-             connected ? resolve('Succesfully connected!') : resolve(`Closed connection with ${errorMessage}`)
-          })
+        const {connected, errorMessage} = await this.mqttService.connect(mqttOptionsDto ? mqttOptionsDto : body.mqttOptionsDto)
+        resolve(connected ? 'You are successfully connected!' : `Error: ${errorMessage.message}`)
       }
     )
-    
     return promise
   }
 
-
+  @UseGuards(AccessTokenGuard)
+  @Get('disconnect')
+  async disconnect(
+    @Req() req: Request
+  ):Promise<string>{
+    if (!this.mqttService.checkConnection()) return `You are not connected.`
+    await this.mqttService.disconnect()
+    return `Connection status: ${this.mqttService.checkConnection()}`
+  }
 
 
 
@@ -50,24 +52,18 @@ export class MqttController {
   async subscribe(
     @Req() req: Request,
     @Body() body: { templateID: string},
-  ): Promise<any> {
-    if (!this.mqttService.checkConnection()) return `You are not connected to the server.`
-      const template = await this.templatesService.findUserTemplateByID(req.user['sub'], body.templateID)
-      const promise = new Promise(
-        async (resolve, reject) => {
-          setTimeout(() => {resolve(`You are haven't recieved any message. The waiting time has expired.`)}, 100000)
-          this.mqttService.subscribe(template.topic, 
-            (message: string) => {
-              this.logger.log(`Received message on ${template.topic}: ${message}`)
-                resolve(`Received message on ${template.topic}: ${message}`)
-            })
-          
-      })
+  ): Promise<string> {
+    if (!this.mqttService.checkConnection()) return `You are not connected.`
+    const template = await this.templatesService.findUserTemplateByID(req.user['sub'], body.templateID)
+    const promise = new Promise<string>(
+      async (resolve, reject) => {
+        setTimeout(() => {resolve(`You are haven't recieved any message. The waiting time has expired.`)}, 100000)
+        resolve(this.mqttService.subscribe(template.topic))
+    })
 
-      promise.then(() => {this.mqttService.unsubscribe(template.topic)})
-
-      return promise
-    }
+    promise.then(() => {this.mqttService.unsubscribe(template.topic)})
+    return promise
+  }
   
   @UseGuards(AccessTokenGuard)
   @Get('/publish')
@@ -75,8 +71,8 @@ export class MqttController {
     @Req() req: Request,
     @Body() body: { templateID: string},
   ): Promise<string> {
-    if (!this.mqttService.checkConnection()) return `You are not connected to the server.`
-    setTimeout(() => {return(`The waiting time has expired. Check the connection to the server.`)}, 10000)
+    if (!this.mqttService.checkConnection()) return `You are not connected.`
+    setTimeout(() => {return(`The waiting time has expired`)}, 10000)
     const template = await this.templatesService.findUserTemplateByID(req.user['sub'], body.templateID)
     this.mqttService.publish(template.topic,template.message);
     return `Published ${template.topic} to ${template.message}`;
